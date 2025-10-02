7fd7ec4c2d4a324f7331310c2b0b214e9709fb62
Purin (Trak) Prateepmanowong <purin.trak@gmail.com>
2025-10-01 20:30:44 -0400

    fix

--- trak-portfolio/src/components/ui/ExploreGate.jsx
+++ trak-portfolio/src/components/ui/ExploreGate.jsx
@@ -7,7 +7,7 @@ export default function ExploreGate({ onClick }) {
   if (offset < 0.95) return null
 
   return (
-    <div className="fixed bottom-20 left-1/2 -translate-x-1/2 z-20">
+    <div className="relative bottom-20 left-1/2 -translate-x-1/2 z-20">
       <ExploreButton onClick={onClick} />
     </div>
   )
